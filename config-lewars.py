76d29d0a31ba1629a2305c1516e82e1c0c3a74af
Alistair Y. Lewars <alistair.lewars@gmail.com>
2022-09-09 10:42:25 -0400

    Fix all keybindings

--- config-lewars.py
+++ config-lewars.py
@@ -40,9 +40,62 @@ define_timeout(1)
 #    Key.RIGHT_SHIFT: [Key.KPRIGHTPAREN, Key.RIGHT_SHIFT]
 # })
 
+# Keybindings for Abricotine
+define_keymap(re.compile("abricotine", flags=re.IGNORECASE), {
+
+    # Newline
+    K("C-m"): K("enter"),
+    K("C-o"): [K("enter"), K("left")],
+    # Copy
+    K("C-y"): [K("C-v"), set_mark(False)],
+    # Non-Emacs cut, copy, and paste
+    K("Super-x"): K("C-x"),
+    K("Super-c"): K("C-c"),
+    K("Super-v"): K("C-v"),
+    K("Super-Shift-v"): K("C-Shift-v"),
+    # Undo
+    K("Super-z"): [K("C-z"), set_mark(False)],
+    K("Super-Shift-z"): [K("C-y"), set_mark(False)],
+    # Mark
+    K("C-space"): set_mark(True),
+    K("C-M-space"): with_or_set_mark(K("C-right")),
+    # Search
+    K("C-s"): K("F3"),
+    K("C-r"): K("Shift-F3"),
+    # Non-Emacs Search
+    K("Super-f"): K("C-f"),
+    K("Super-g"): K("Shift-F3"),
+    K("Super-Shift-g"): K("C-Shift-g"),
+    # Non-Emacs Print
+    K("Super-p"): K("C-p"),
+    # Cancel
+    K("C-g"): [K("esc"), set_mark(False)],
+    # Italic, Bold, Strike-through
+    K("Super-i"): K("C-i"),
+    K("Super-b"): K("C-b"),
+
+    # C-x YYY
+    K("C-x"): {
+    #     # C-x h (select all)
+    #     K("h"): [K("C-home"), K("C-a"), set_mark(True)],
+    #     # C-x C-f (open)
+    #     K("C-f"): K("C-o"),
+    #     # C-x C-s (save)
+    #     K("C-s"): K("C-s"),
+    #     # C-x k (kill tab)
+    #     K("k"): K("C-f4"),
+    #     # C-x C-c (exit)
+    #     K("C-c"): K("C-q"),
+    #     # cancel
+    #     K("C-g"): pass_through_key,
+    #     # C-x u (undo)
+    #     K("u"): [K("C-z"), set_mark(False)],
+    }
+
+}, "Abricotine")
 
 # Keybindings for Firefox/Chrome
-define_keymap(re.compile("Firefox|Google-chrome", flags=re.IGNORECASE), {
+define_keymap(re.compile("Firefox|Google-chrome|Brave-browser", flags=re.IGNORECASE), {
     # Tab
     K("Super-t"): K("C-t"), # open tab
     K("Super-w"): K("C-w"), # close tab
@@ -54,24 +107,9 @@ define_keymap(re.compile("Firefox|Google-chrome", flags=re.IGNORECASE), {
     # # very naive "Edit in editor" feature (just an example)
     # K("C-o"): [K("C-a"), K("C-c"), launch(["gedit"]), sleep(0.5), K("C-v")]
 
-    # Cursor
-    K("C-b"): with_mark(K("left")),
-    K("C-f"): with_mark(K("right")),
-    K("C-p"): with_mark(K("up")),
-    K("C-n"): with_mark(K("down")),
-    # K("C-h"): with_mark(K("backspace")),
-    # Forward/Backward word
-    K("M-b"): with_mark(K("C-left")),
-    K("M-f"): with_mark(K("C-right")),
-    # Beginning/End of line
-    K("C-a"): with_mark(K("home")),
-    K("C-e"): with_mark(K("end")),
     # Page up/down
     K("M-v"): with_mark(K("page_up")),
     # K("C-v"): with_mark(K("page_down")),
-    # Beginning/End of file
-    K("M-Shift-comma"): with_mark(K("C-home")),
-    K("M-Shift-dot"): with_mark(K("C-end")),
     # Newline
     K("C-m"): K("enter"),
     K("C-j"): K("enter"),
@@ -84,14 +122,10 @@ define_keymap(re.compile("Firefox|Google-chrome", flags=re.IGNORECASE), {
     K("Super-x"): K("C-x"),
     K("Super-c"): K("C-c"),
     K("Super-v"): K("C-v"),
+    K("Super-Shift-v"): K("C-Shift-v"),
     # Delete
     K("C-d"): [K("delete"), set_mark(False)],
     K("M-d"): [K("C-delete"), set_mark(False)],
-    # Kill line
-    K("C-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
-    # Undo
-    K("Super-z"): [K("C-z"), set_mark(False)],
-    K("Super-Shift-z"): [K("C-y"), set_mark(False)],
     # K("C-slash"): [K("C-z"), set_mark(False)],
     # K("C-Shift-ro"): K("C-z"),
     # Mark
@@ -114,21 +148,42 @@ define_keymap(re.compile("Firefox|Google-chrome", flags=re.IGNORECASE), {
     K("C-g"): [K("esc"), set_mark(False)],
     # Escape
     K("C-q"): escape_next_key,
-    # C-x YYY
-    # K("C-x"): {
-    #     # C-x h (select all)
-    #     K("h"): [K("C-home"), K("C-a"), set_mark(True)],
-    #     # C-x C-f (open)
-    #     K("C-f"): K("C-o"),
-    #     # C-x C-s (save)
-    #     K("C-s"): K("C-s"),
-    #     # C-x k (kill tab)
-    #     K("k"): K("C-f4"),
-    #     # C-x C-c (exit)
-    #     K("C-c"): K("C-q"),
-    #     # cancel
-    #     K("C-g"): pass_through_key,
-    #     # C-x u (undo)
-    #     K("u"): [K("C-z"), set_mark(False)],
-    # }
+
+    # New Window/Incognito Window
+    K("Super-n"): K("C-n"),
+    K("Super-Shift-n"): K("C-Shift-n"),
 }, "Firefox and Chrome")
+
+# Global keybindings
+define_keymap(lambda wm_class: wm_class not in ("Emacs"), {
+    # Cut, copy, and paste
+    K("Super-x"): K("C-x"),
+    K("Super-c"): K("C-c"),
+    K("Super-v"): K("C-v"),
+    # Cursor
+    K("C-b"): with_mark(K("left")),
+    K("C-f"): with_mark(K("right")),
+    K("C-p"): with_mark(K("up")),
+    K("C-n"): with_mark(K("down")),
+    # Forward/Backward word
+    K("M-b"): with_mark(K("C-left")),
+    K("M-f"): with_mark(K("C-right")),
+    # Beginning/End of line
+    K("C-a"): with_mark(K("home")),
+    K("C-e"): with_mark(K("end")),
+    # Newline
+    K("C-o"): [K("enter"), K("left")],
+    # Undo
+    K("Super-z"): [K("C-z"), set_mark(False)],
+    K("Super-Shift-z"): [K("C-y"), set_mark(False)],
+    # Copy
+    K("C-y"): [K("C-v"), set_mark(False)],
+    # Kill line
+    K("C-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
+    # Quit
+    K("Super-q"): K("C-q"),
+}, "Global Keybindings")
+
+# Re-Keybind for Firefox/Chrome/Brave
+define_keymap(re.compile("Firefox|Google-chrome|Brave-browser", flags=re.IGNORECASE), {
+}, "Re-key Firefox and Chrome browsers")
