f6add951d5198f6521aaf5576835749593c62e36
Alistair Y. Lewars <alistair.lewars@gmail.com>
2022-10-05 14:35:12 -0400

    Update

--- config-lewars.py
+++ config-lewars.py
@@ -41,95 +41,100 @@ define_timeout(1)
 # })
 
 # Keybindings for Firefox/Chrome
-define_keymap(re.compile("Firefox|Google-chrome|Brave-browser", flags=re.IGNORECASE), {
-    # Tab
-    K("Super-t"): K("C-t"), # open tab
-    K("Super-w"): K("C-w"), # close tab
-    K("Super-Shift-RIGHT_BRACE"): K("C-TAB"),
-    K("Super-Shift-LEFT_BRACE"): K("C-Shift-TAB"),
-
-    # # Type C-j to focus to the content
-    # K("C-j"): K("C-f6"),
-    # # very naive "Edit in editor" feature (just an example)
-    # K("C-o"): [K("C-a"), K("C-c"), launch(["gedit"]), sleep(0.5), K("C-v")]
-
-    # Page up/down
-    K("M-v"): with_mark(K("page_up")),
-    # K("C-v"): with_mark(K("page_down")),
-    # Newline
-    K("C-m"): K("enter"),
-    K("C-j"): K("enter"),
-    K("C-o"): [K("enter"), K("left")],
-    # Copy
-    # K("C-w"): [K("C-x"), set_mark(False)],
-    # K("M-w"): [K("C-c"), set_mark(False)],
-    K("C-y"): [K("C-v"), set_mark(False)],
-    # Select all
-    K("Super-a"): K("C-a"),
-    # Non-Emacs cut, copy, and paste
-    K("Super-x"): K("C-x"),
-    K("Super-c"): K("C-c"),
-    K("Super-v"): K("C-v"),
-    K("Super-Shift-v"): K("C-Shift-v"),
-    # Delete
-    K("C-d"): [K("delete"), set_mark(False)],
-    K("M-d"): [K("C-delete"), set_mark(False)],
-    # K("C-slash"): [K("C-z"), set_mark(False)],
-    # K("C-Shift-ro"): K("C-z"),
-    # Mark
-    K("C-space"): set_mark(True),
-    K("C-M-space"): with_or_set_mark(K("C-right")),
-    # Search
-    K("C-s"): K("F3"),
-    K("C-r"): K("Shift-F3"),
-    K("M-Shift-key_5"): K("C-h"),
-    # Non-Emacs Search
-    K("Super-f"): K("C-f"),
-    K("Super-g"): K("C-g"),
-    K("Super-Shift-g"): K("C-Shift-g"),
-    # Non-Emacs Print
-    K("Super-p"): K("C-p"),
-    # Non-Emacs Reload
-    K("Super-r"): K("C-r"),
-    K("Super-Shift-r"): K("C-Shift-r"),
-    # Cancel
-    K("C-g"): [K("esc"), set_mark(False)],
-    # Escape
-    K("C-q"): escape_next_key,
-
-    # New Window/Incognito Window
-    K("Super-n"): K("C-n"),
-    K("Super-Shift-n"): K("C-Shift-n"),
-}, "Firefox and Chrome")
+define_keymap(
+    re.compile("Firefox|Google-chrome|Brave-browser", flags=re.IGNORECASE),
+    {
+        # Tab
+        K("Super-t"): K("C-t"),  # open tab
+        K("Super-w"): K("C-w"),  # close tab
+        K("Super-Shift-RIGHT_BRACE"): K("C-TAB"),
+        K("Super-Shift-LEFT_BRACE"): K("C-Shift-TAB"),
+        # # Type C-j to focus to the content
+        # K("C-j"): K("C-f6"),
+        # # very naive "Edit in editor" feature (just an example)
+        # K("C-o"): [K("C-a"), K("C-c"), launch(["gedit"]), sleep(0.5), K("C-v")]
+        # Page up/down
+        K("M-v"): with_mark(K("page_up")),
+        # K("C-v"): with_mark(K("page_down")),
+        # Newline
+        K("C-m"): K("enter"),
+        K("C-j"): K("enter"),
+        K("C-o"): [K("enter"), K("left")],
+        # Copy
+        # K("C-w"): [K("C-x"), set_mark(False)],
+        # K("M-w"): [K("C-c"), set_mark(False)],
+        K("C-y"): [K("C-v"), set_mark(False)],
+        # Select all
+        K("Super-a"): K("C-a"),
+        # Non-Emacs cut, copy, and paste
+        K("Super-x"): K("C-x"),
+        K("Super-c"): K("C-c"),
+        K("Super-v"): K("C-v"),
+        K("Super-Shift-v"): K("C-Shift-v"),
+        # Delete
+        K("C-d"): [K("delete"), set_mark(False)],
+        K("M-d"): [K("C-delete"), set_mark(False)],
+        # K("C-slash"): [K("C-z"), set_mark(False)],
+        # K("C-Shift-ro"): K("C-z"),
+        # Mark
+        K("C-space"): set_mark(True),
+        K("C-M-space"): with_or_set_mark(K("C-right")),
+        # Search
+        K("C-s"): K("F3"),
+        K("C-r"): K("Shift-F3"),
+        K("M-Shift-key_5"): K("C-h"),
+        # Non-Emacs Search
+        K("Super-f"): K("C-f"),
+        K("Super-g"): K("C-g"),
+        K("Super-Shift-g"): K("C-Shift-g"),
+        # Non-Emacs Print
+        K("Super-p"): K("C-p"),
+        # Non-Emacs Reload
+        K("Super-r"): K("C-r"),
+        K("Super-Shift-r"): K("C-Shift-r"),
+        # Cancel
+        K("C-g"): [K("esc"), set_mark(False)],
+        # Escape
+        K("C-q"): escape_next_key,
+        # New Window/Incognito Window
+        K("Super-n"): K("C-n"),
+        K("Super-Shift-n"): K("C-Shift-n"),
+    },
+    "Firefox and Chrome",
+)
 
 # Global keybindings
-define_keymap(lambda wm_class: wm_class not in ("Emacs", "gnome-terminal-server", "Gnome-terminal", "gnome-boxes"), {
-    # Cut, copy, and paste
-    K("Super-x"): K("C-x"),
-    K("Super-c"): K("C-c"),
-    K("Super-v"): K("C-v"),
-    # Cursor
-    K("C-b"): with_mark(K("left")),
-    K("C-f"): with_mark(K("right")),
-    K("C-p"): with_mark(K("up")),
-    K("C-n"): with_mark(K("down")),
-    # Forward/Backward word
-    K("M-b"): with_mark(K("C-left")),
-    K("M-f"): with_mark(K("C-right")),
-    # Select all
-    K("Super-a"): K("C-a"),
-    # Beginning/End of line
-    K("C-a"): with_mark(K("home")),
-    K("C-e"): with_mark(K("end")),
-    # Newline
-    K("C-o"): [K("enter"), K("left")],
-    # Undo
-    K("Super-z"): [K("C-z"), set_mark(False)],
-    K("Super-Shift-z"): [K("C-y"), set_mark(False)],
-    # Copy
-    K("C-y"): [K("C-v"), set_mark(False)],
-    # Kill line
-    K("C-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
-    # Quit
-    K("Super-q"): K("C-q"),
-}, "Global Keybindings")
+define_keymap(
+    lambda wm_class: wm_class not in ("Emacs", "gnome-terminal-server", "Gnome-terminal", "gnome-boxes", "jetbrains-idea"),
+    {
+        # Cut, copy, and paste
+        K("Super-x"): K("C-x"),
+        K("Super-c"): K("C-c"),
+        K("Super-v"): K("C-v"),
+        # Cursor
+        K("C-b"): with_mark(K("left")),
+        K("C-f"): with_mark(K("right")),
+        K("C-p"): with_mark(K("up")),
+        K("C-n"): with_mark(K("down")),
+        # Forward/Backward word
+        K("M-b"): with_mark(K("C-left")),
+        K("M-f"): with_mark(K("C-right")),
+        # Select all
+        K("Super-a"): K("C-a"),
+        # Beginning/End of line
+        K("C-a"): with_mark(K("home")),
+        K("C-e"): with_mark(K("end")),
+        # Newline
+        K("C-o"): [K("enter"), K("left")],
+        # Undo
+        K("Super-z"): [K("C-z"), set_mark(False)],
+        K("Super-Shift-z"): [K("C-y"), set_mark(False)],
+        # Copy
+        K("C-y"): [K("C-v"), set_mark(False)],
+        # Kill line
+        K("C-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
+        # Quit
+        K("Super-q"): K("C-q"),
+    },
+    "Global Keybindings",
+)
